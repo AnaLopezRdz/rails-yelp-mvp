85a75145a21181a1f79d0243ed1fe03bf21bebde
analopezrdz <annalucable@gmail.com>
2023-07-28 20:54:38 +0200

    nested models... dont forget to validate all the values in both models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'revies/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews
+    collection do
+      get :japanese
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# puts "cleanins database"
+# Restaurant.destroy_all
+puts 'creating restaurants'
+Restaurant.create(name: 'pizzeria', address: 'javaastrat 101', phone_number: '06 14 95 65 32', category: 'italian')
+Restaurant.create(name: 'momiji', address: 'princessstrat 39', phone_number: '06 14 95 35 34', category: 'japanese')
+Restaurant.create(name: 'baguette', address: 'javaastrat 101', phone_number: '06 36 35 12 32', category: 'french')
+Restaurant.create(name: 'chilte', address: 'princessstrat 39', phone_number: '06 14 95 35 34', category: 'japanese')
+Restaurant.create(name: 'little v', address: 'princessstrat 39', phone_number: '06 14 95 35 34', category: 'japanese')
+puts 'finish'
